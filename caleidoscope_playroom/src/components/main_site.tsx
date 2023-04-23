80b123d3267ea1ad728b1e6d0862b4043119adac
Marek Rogiński <marekroginski1994@gmail.com>
2023-04-24 00:05:52 +0200

    mainsite, first part of disign

---
+++ caleidoscope_playroom/src/components/main_site.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Navbar } from "./nav";
+export function Mainsite(){
+    return(
+<div className="mainsite"><Navbar></Navbar></div>
+
+    );
+}
---
+++ caleidoscope_playroom/src/components/nav.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export function Navbar (){
+    return(
+        
+        <ul className="navbar">
+            <li>Sala zabaw kalejdoskop</li>
+            <li>Atrakcje</li>
+            <li>urodzinki</li>
+            <li>Kontakt</li>
+        </ul>
+        
+    )
+}
