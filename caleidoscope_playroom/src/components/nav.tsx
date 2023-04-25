7738f9f8df18d406ad3f89d31c5ce573c5761f88
Marek Rogiński <marekroginski1994@gmail.com>
2023-04-25 23:56:38 +0200

    style and create dropdown menu

--- caleidoscope_playroom/src/components/nav.tsx
+++ caleidoscope_playroom/src/components/nav.tsx
@@ -3,12 +3,33 @@ import React from "react";
 export function Navbar (){
     return(
         
-        <ul className="navbar">
-            <li>Sala zabaw kalejdoskop</li>
-            <li>Atrakcje</li>
-            <li>urodzinki</li>
-            <li>Kontakt</li>
-        </ul>
+        <nav className="navbar">
+            <div className="dropdown">
+            <ul><a href="#"> Sala zabaw Kalejdoskop</a>
+                <li><a href="#">Historia</a></li>
+                <li><a href="#">Regulamin Sali</a></li>
+                <li><a href="#">Polityka prywatności</a></li>
+            </ul>
+            <ul><a href="#">Atrakcje</a>
+                <li><a href="#">Konstrukcja zabawowa</a></li>
+                <li><a href="#">Strefa malucha</a></li>
+                <li><a href="#">Strefa urodzinowa</a></li>
+                <li><a href="#">Słodka strefa</a></li>
+            </ul>
+            <ul><a href="#">Urodzinki</a>
+                <li><a href="#">Konstrukcja zabawowa</a></li>
+                <li><a href="#">Strefa malucha</a></li>
+                <li><a href="#">Strefa urodzinowa</a></li>
+                <li><a href="#">Słodka strefa</a></li>
+            </ul>
+            <ul><a href="#">Kontakt</a>
+            <li><a href="#">Konstrukcja zabawowa</a></li>
+                <li><a href="#">Strefa malucha</a></li>
+                <li><a href="#">Strefa urodzinowa</a></li>
+                <li><a href="#">Słodka strefa</a></li>
+            </ul>
+            </div>
+        </nav>
         
     )
 }
